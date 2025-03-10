69e3f2673e4b8546ff59361c408db27d7db9703e
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-03-11 01:13:56 +0300

    Initial Commit

---
+++ Folder vs Groups/AppEntry.swift
@@ -0,0 +1,17 @@
+//
+//  Folder_vs_GroupsApp.swift
+//  Folder vs Groups
+//
+//  Created by Ahmet Bostancıklıoğlu on 11.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppEntry: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
